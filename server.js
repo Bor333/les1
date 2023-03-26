1ad9294e32bae8ecc5c79333701a4386549bb51c
Bor333 <mugen999999999@gmail.com>
2023-03-26 14:13:50 +0700

    console.log(file);

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
 
 const host = 'localhost';
 const port = 8000;
@@ -8,21 +9,31 @@ const requestListener = (req, res) => {
     
     if (req.method !== 'GET') {
         res.writeHead(405);
-        res.end('HTTP method not allowed');
-    } else {
-        if (req.url === '/geeekbrains') {
-            res.writeHead(200);
-            res.end('Success!');
-        }
-    
-        if (req.url === '/example') {
-            res.writeHead(200);
-            res.end('Success again!');
-        }
-    
+        return res.end('HTTP method not allowed');      
+    } 
+
+    if (req.url === '/geeekbrains') {
+        fs.readdir(__dirname + '/files/', (err, files) => {
+            if (err)
+              console.log(err);
+            else {
+              console.log("\nCurrent directory filenames:");
+              files.forEach(file => {
+                console.log(file);
+              })
+            }
+          })
         res.writeHead(200);
-        res.end('Привет! Это мой первый HTTP-сервер');
+        return res.end('Success!');
     }
+
+    if (req.url === '/example') {
+        res.writeHead(200);
+        res.end('Success again!');
+    }
+
+    res.writeHead(200);
+    res.end('Привет! Это мой первый HTTP-сервер');
 }
 
 const server = http.createServer(requestListener);
