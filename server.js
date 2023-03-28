14e01ab0a344a05bfb480351a3eb6350e3797069
Bor333 <mugen999999999@gmail.com>
2023-03-28 13:09:28 +0700

    hw1 finish

--- server.js
+++ server.js
@@ -5,35 +5,74 @@ const fs = require('fs');
 const host = 'localhost';
 const port = 8000;
 
+
+
 const requestListener = (req, res) => {
-    
-    if (req.method !== 'GET') {
-        res.writeHead(405);
-        return res.end('HTTP method not allowed');      
-    } 
 
-    if (req.url === '/geeekbrains') {
+    if (req.url === '/get') {
+
+        if (req.method !== 'GET') {
+            res.writeHead(405);
+            return res.end('HTTP method not allowed!');
+        }
+
+        let output = '';
+
         fs.readdir(__dirname + '/files/', (err, files) => {
-            if (err)
-              console.log(err);
-            else {
-              console.log("\nCurrent directory filenames:");
-              files.forEach(file => {
-                console.log(file);
-              })
+            if (err) {
+                res.writeHead(500);
+                return res.end('Internal server error!');
             }
-          })
-        res.writeHead(200);
-        return res.end('Success!');
+            files.forEach(file => {
+                output += file + ', ';
+            })
+
+            output = output.slice(0, -2);
+
+            res.writeHead(200);
+            res.end(output);
+        })
+
+        return;
     }
 
-    if (req.url === '/example') {
-        res.writeHead(200);
-        res.end('Success again!');
+    if (req.url === '/delete') {
+
+        if (req.method === 'DELETE') {
+            res.writeHead(200);
+            res.end('Success!');
+        } else {
+            res.writeHead(405);
+            res.end('HTTP method not allowed!');
+        }
+        return;
+
     }
 
-    res.writeHead(200);
-    res.end('Привет! Это мой первый HTTP-сервер');
+    if (req.url === '/post') {
+
+        if (req.method === 'POST') {
+            res.writeHead(200);
+            res.end('Success!');
+        } else {
+            res.writeHead(405);
+            res.end('HTTP method not allowed!');
+        }
+        return;
+    }
+
+    if (req.url === '/redirect') {
+
+        if (req.method === 'GET') {
+            res.writeHead(200);
+            res.end(`Ресурс теперь постоянно доступен по адресу /redirected`);
+        } else {
+            res.writeHead(405);
+            res.end('HTTP method not allowed!');
+        }
+        return;        
+    }
+
 }
 
 const server = http.createServer(requestListener);
